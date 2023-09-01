0239e11ee0a1003a2557bdc75f563e011ef57283
David <zhaocx@mail.dlut.edu.cn>
2023-09-01 16:57:49 +0800

    my config file including plugins

--- init.lua
+++ init.lua
@@ -17,7 +17,10 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = require("custom_keys").leader
 vim.g.maplocalleader = "\\"
-
+vim.g.encoding="utf-8"
+vim.o.fileencodings="utf-8"
+vim.g.goyo_height="95%"
+vim.wo.wrap = true
 require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
@@ -32,7 +35,33 @@ require("lazy").setup({
 		notify = false, -- get a notification when changes are found
 	},
 })
-
+require('bookmarks').setup {
+	-- sign_priority = 8,  --set bookmark sign priority to cover other sign
+	save_file = vim.fn.expand "$HOME/.bookmarks", -- bookmarks save file path
+	keywords =  {
+	  ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
+	  ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
+	  ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+	  ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
+	},
+	on_attach = function(bufnr)
+	  local bm = require "bookmarks"
+	  local map = vim.keymap.set
+	  map("n","mm",bm.bookmark_toggle) -- add or remove bookmark at current line
+	  map("n","mi",bm.bookmark_ann) -- add or edit mark annotation at current line
+	  map("n","mc",bm.bookmark_clean) -- clean all marks in local buffer
+	  map("n","mn",bm.bookmark_next) -- jump to next mark in local buffer
+	  map("n","mp",bm.bookmark_prev) -- jump to previous mark in local buffer
+	  map("n","ml",bm.bookmark_list) -- show marked file list in quickfix window
+	end
+}
+require'lspconfig'.typst_lsp.setup{
+	settings = {
+		exportPdf = "onSave" -- Choose onType, onSave or never.
+        -- serverPath = "" -- Normally, there is no need to uncomment it.
+	}
+}
+vim.filetype.add({ extension = {typ = "typst"}})
 -- Final settings
 require("core")
 pcall(require, "custom")
---
+++ lua/my_plugins/plugin_name.lua
@@ -0,0 +1,88 @@
+return {
+    -- Your Plugin1
+    {
+        'JuliaEditorSupport/julia-vim',
+    },
+    {
+    "hkupty/iron.nvim",
+      config = function(plugins, opts)
+        local iron = require("iron.core")
+
+        iron.setup({
+          config = {
+            -- Whether a repl should be discarded or not
+            scratch_repl = true,
+            -- Your repl definitions come here
+            repl_definition = {
+              python = {
+                -- Can be a table or a function that
+                -- returns a table (see below)
+                command = { "python" },
+              },
+            },
+            -- How the repl window will be displayed
+            -- See below for more information
+            repl_open_cmd = require("iron.view").right(60),
+          },
+          -- Iron doesn't set keymaps by default anymore.
+          -- You can set them here or manually add keymaps to the functions in iron.core
+          keymaps = {
+            send_motion = "<space>rc",
+            visual_send = "<space>rc",
+            send_file = "<space>rf",
+            send_line = "<space>rl",
+            send_mark = "<space>rm",
+            mark_motion = "<space>rmc",
+            mark_visual = "<space>rmc",
+            remove_mark = "<space>rmd",
+            cr = "<space>r<cr>",
+            interrupt = "<space>r<space>",
+            exit = "<space>rq",
+            clear = "<space>rx",
+          },
+          -- If the highlight is on, you can change how it looks
+          -- For the available options, check nvim_set_hl
+          highlight = {
+            italic = true,
+          },
+          ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+        })
+
+        -- iron also has a list of commands, see :h iron-commands for all available commands
+        vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
+        vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
+        vim.keymap.set("n", "<space>rF", "<cmd>IronFocus<cr>")
+        vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
+      end,
+    },
+    {
+      'kaarmu/typst.vim',
+    },
+    {
+      'lervag/vimtex',
+      opt = true,
+      config = function ()
+        vim.g.vimtex_view_general_viewer = 'sumatrapdf.exe'
+        vim.g.vimtex_compiler_latexmk_engines = {
+            _ = '-xelatex'
+        }
+        vim.g.tex_comment_nospell = 1
+        vim.g.vimtex_compiler_progname = 'nvr'
+        vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
+        vim.g.vimtex_view_general_options_latexmk = '--unique'
+    end,
+    ft = 'tex'
+    },
+    {
+      'tomasky/bookmarks.nvim'
+    },
+    {
+      'junegunn/limelight.vim'
+    },
+    {
+      'junegunn/goyo.vim'
+    },
+    {
+      'honza/vim-snippets'
+    },
+}
