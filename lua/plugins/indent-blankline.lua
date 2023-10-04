eb4d06b300d0f6776ab0a44eaf26542919b7f41f
David-Alfred <zhaochenxialone@foxmail.com>
2023-10-04 23:07:10 +0800

    convert indent-blankline from version 2 to version 3

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,18 +1,8 @@
 return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		main="ibl",
 		opts = {
-			space_char_blankline = " ",
-			show_current_context = true,
-			show_current_context_start = true,
-			char_highlight_list = {
-				"IndentBlanklineIndent1",
-				"IndentBlanklineIndent2",
-				"IndentBlanklineIndent3",
-				"IndentBlanklineIndent4",
-				"IndentBlanklineIndent5",
-				"IndentBlanklineIndent6",
-			},
 		},
 	},
 }
